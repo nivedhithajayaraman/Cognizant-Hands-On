d8eae35f078401e0c7c51d204367a3adda9218f2
nivedhithajayaraman <125451624+nivedhithajayaraman@users.noreply.github.com>
2025-07-13 15:28:17 +0530

    Add files via upload

---
+++ WEEK 4 COGNIZANT/hello-world-services/hello-world-services/src/test/java/com/example/hello_world_services/HelloWorldServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_world_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
